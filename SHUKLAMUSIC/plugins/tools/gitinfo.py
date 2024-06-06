2d49c76c36d1fe6389d2dca55918b94e463e898a
JaishreeRamTG <151377049+JaishreeRamTG@users.noreply.github.com>
2024-06-06 15:03:40 +0530

    Update gitinfo.py

--- SHUKLAMUSIC/plugins/tools/gitinfo.py
+++ SHUKLAMUSIC/plugins/tools/gitinfo.py
@@ -1,7 +1,7 @@
 import asyncio, os, time, aiohttp
 import aiohttp
 from pyrogram import filters
-from daxxhub import daxxhub as papadaxx
+#from daxxhub import daxxhub as papadaxx
 from SHUKLAMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
